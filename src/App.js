8082a9bf3fe44ccb915cdfb883678a25f23a63fe
shayaan-git <mshayaan24@gmail.com>
2025-07-15 19:29:04 +0530

    modified files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from './magSearch.svg';
 import MovieCard from './MovieCard';
 import { useState} from "react";
 
-const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
+const API_URL = `https://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
 
 
 const App = () => {
